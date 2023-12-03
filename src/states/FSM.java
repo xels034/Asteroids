9a07dd8a47cbed8f9dad5cb34dc8759169d717f0
Xels034 <59102313+xels034@users.noreply.github.com>
2023-12-03 12:42:25 +0100

    - Fix warnings for latest java version

--- src/states/FSM.java
+++ src/states/FSM.java
@@ -1,11 +1,11 @@
 package states;
 
 /**
- * 
+ *
  * @author David-Peter Desch, Dominik Lisowski
- * 
+ *
  * The finite state machine handles the different states.
- * 
+ *
  */
 
 import java.util.HashMap;
@@ -33,7 +33,7 @@ public class FSM extends State {
       currentState.activate();
     }
   }
-  
+
   /**
    * Only the current States will be updated.
    */
@@ -41,7 +41,7 @@ public class FSM extends State {
   public void update(){
     if(currentState != null)currentState.update();
   }
-  
+
   /**
    * Deactivate the current state and unsubscribe him from the messenger.
    * So the garbage collector can do his work.
@@ -52,33 +52,33 @@ public class FSM extends State {
     if(currentState != null)currentState.deactivate();
     Messenger.unsubscribe(this);
   }
-  
+
   public void registerState(State newState){
-    states.put(newState, new HashMap<Message.STATE, State>());
+    states.put(newState, new HashMap<>());
     SimpleLogger.log("new State added.", 1, this.getClass(), "registerState");
   }
-  
+
   public void registerTransition(State source, Message.STATE menu, State target){
     if(states.containsKey(source)){
       states.get(source).put(menu, target);
       SimpleLogger.log("Registered Transition.", 1, this.getClass(), "registerTransition");
     }
   }
-  
+
   public void unregisterState(State deleteState){
     if(states.containsKey(deleteState)){
       states.remove(deleteState);
       SimpleLogger.log("State deleted.", 1, this.getClass(), "unregisterState");
     }
   }
-  
+
   public void unregisterTransition(State source, Message msg){
     if(states.containsKey(source) && states.get(source).containsKey(msg.getParam())) {
       states.get(source).remove(msg.getParam());
       SimpleLogger.log("Deleted Transition.", 1, this.getClass(), "unregisterTransition");
     }
   }
-  
+
   /**
    * This method sets the start and end state. This two states will be registered
    * if you use this method.
@@ -93,7 +93,7 @@ public class FSM extends State {
     currentState.activate();
     this.activate();
   }
-  
+
   /**
    * Activates the state, so the state listening too his transitions.
    */
